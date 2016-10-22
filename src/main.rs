8f9b87fab8f14e0ea0e11364057fd7f9f0f86f01
anziem <shopper137@gmail.com>
2016-10-22 15:41:57 -0400

    Added ability to take in url from command line.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+extern crate hyper;
+
+use hyper::Client;
+use std::env;
+use std::io::Read;
+
 fn main() {
-    println!("Hello, world!");
+    let client = Client::new();
+
+    let url = &env::args().nth(1).expect("No url supplied");  // in rust 1.1 env::args() returns an Iterator
+
+    let mut res = client.get(url)
+                    .send()
+                    .expect("Request failed");
+
+    let mut body = String::new(); // read_to_string needs &str to store res in
+
+    res.read_to_string(&mut body).expect("Read failed");
+
+    println!("{:?}", body);
+
 }
